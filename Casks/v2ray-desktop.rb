671d7e64f92df67d04b1606a5309b8112ab288ff
microld <2627500295@qq.com>
2020-07-02 21:04:28 +0800

    feat: update v2ray-desktop v2.1.6

--- Casks/v2ray-desktop.rb
+++ Casks/v2ray-desktop.rb
@@ -1,6 +1,6 @@
 cask 'v2ray-desktop' do
-  version '2.1.5'
-  sha256 'd8650e475e7c994e06120582529dea9ad4fa6f01cfc9c41a30bdafc6a6efa179'
+  version '2.1.6'
+  sha256 '8910fc10280d39f8c6cf81592ec81094ba4b6637dbe487d1c0b54967146fc810'
   url "https://github.com/Dr-Incognito/V2Ray-Desktop/releases/download/#{version}/V2Ray-Desktop-v#{version}-macOS.dmg"
   appcast 'https://github.com/Dr-Incognito/V2Ray-Desktop/releases.atom'
   name 'V2Ray-Desktop'
