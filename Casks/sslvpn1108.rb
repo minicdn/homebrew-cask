e146acfa1ccd54cf00e0d2371d15e740470cc9f0
microld <2627500295@qq.com>
2020-12-20 13:54:53 +0800

    Delete sslvpn1108.rb

--- Casks/sslvpn1108.rb
+++
@@ -1,9 +0,0 @@
-cask 'sslvpn1108' do
-  name 'SSLVPN-1108'
-  version '1.01108'
-  homepage 'http://cdn-speed.qiniudn.com'
-  url "http://cdn-speed.qiniudn.com/SSLVPN/Mac/RJSSLVPN-#{version}.dmg"
-  sha256 'b067074e1a8944271c349ce4be2828f28aaf7629233c3fb9cf3864f1b9139d01'
-  # depends_on macos: '>= :sierra'
-  app 'SSLVPN Client.app'
-end
