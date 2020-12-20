2283a4e7c7af47ad567bb985b5d5efe4329cf451
microld <2627500295@qq.com>
2020-12-20 13:51:14 +0800

    Update and rename sslvpn.rb to rjsslvpn.rb

--- Casks/rjsslvpn.rb
+++ Casks/rjsslvpn.rb
@@ -1,11 +1,8 @@
-cask 'sslvpn' do
-  name 'SSLVPN'
+cask 'rjsslvpn' do
+  name 'RJSSLVPN'
   version '1.01109'
   homepage 'http://cdn-speed.qiniudn.com'
   url "http://cdn-speed.qiniudn.com/SSLVPN/Mac/RJSSLVPN-#{version}.dmg"
   sha256 '3c7261bdb3647ed32a51e2697100fa3855ef1b1269262f0882987d92659c0de5'
-  # depends_on macos: '>= :sierra'
   app 'SSLVPN Client.app'
 end
-
-# shasum -a 256 <filePath>
