bd9cbc75f01412cbe9784f747b9868afae1efa31
microld <2627500295@qq.com>
2020-12-22 20:03:38 +0800

    Update rjsslvpn.rb

--- Casks/rjsslvpn.rb
+++ Casks/rjsslvpn.rb
@@ -1,8 +1,8 @@
 cask 'rjsslvpn' do
   name 'RJSSLVPN'
   version '1.01109'
-  homepage 'http://cdn-speed.qiniudn.com'
-  url "http://cdn-speed.qiniudn.com/SSLVPN/Mac/RJSSLVPN-#{version}.dmg"
+  homepage 'http://rgos.ruijie.com.cn/rjvpn/'
   sha256 '3c7261bdb3647ed32a51e2697100fa3855ef1b1269262f0882987d92659c0de5'
+  url "http://cdn-speed.qiniudn.com/Software/MacOS/RJSSLVPN/#{version}/RJSSLVPN.dmg"
   app 'SSLVPN Client.app'
 end
